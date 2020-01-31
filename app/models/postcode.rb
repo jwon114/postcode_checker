b291f8621eb1ab81c6e1cecc49a231ef60e810ce
jwon114 <jthgwong@gmail.com>
2020-01-31 22:51:45 +1300

    update postcode model tests

--- app/models/postcode.rb
+++ app/models/postcode.rb
@@ -1,18 +1,18 @@
+require 'net/http'
+require 'pry-rails'
+
 class Postcode
   URL = 'http://postcodes.io'
-  attr_reader :result, :postcode
+  attr_reader :result
 
   def initialize(postcode)
-    @result = nil
-    @postcode = postcode
+    uri = URI("#{URL}/postcodes/#{postcode}")
+    @response = Net::HTTP.get_response(uri)
+    response_body = JSON.parse(@response.body)
+    @result = is_valid? ? response_body["result"] : response_body
   end
 
   def is_valid?
-  end
-
-  private
-
-  def build_postcode_url(postcode)
-    return "#{URL}/postcodes/#{postcode}"
+    return @response.kind_of?(Net::HTTPSuccess)
   end
 end
--- test/models/postcode_test.rb
+++ test/models/postcode_test.rb
@@ -5,16 +5,15 @@ require 'pry-rails'
 class PostcodeTest < ActiveSupport::TestCase
   test '#is_valid? - API response 404 status, invalid postcode' do
     postcode = Postcode.new('invalid_postcode')
-    postcode.is_valid?
     
-    assert_response :missing
+    assert_not postcode.is_valid?
+    assert_equal "Invalid postcode", postcode.result["error"]
   end
 
   test '#is_valid? - API response contains LSOA Southwark' do
     postcode = Postcode.new('SE17QD')
-    postcode.is_valid?
     
-    assert_response :success
+    assert postcode.is_valid?
     assert postcode.result["lsoa"].start_with?('Southwark')
   end
 
@@ -22,12 +21,19 @@ class PostcodeTest < ActiveSupport::TestCase
     postcode = Postcode.new('SE17QA')
     postcode.is_valid?
 
-    assert_response :success
+    assert postcode.is_valid?
     assert postcode.result["lsoa"].start_with?('Lambeth')
   end
 
-  # test '#validate - valid postcode input' do
-  #   postcode = 'SE17QD'
+  test '#input_valid? - valid postcode input' do
+    postcode = Postcode.new('SE17QD')
 
-  # end
+    assert postcode.input_valid?
+  end
+
+  test '#input_valid? - invalid postcode input' do
+    postcode = Postcode.new('!"#$%^')
+
+    assert_not postcode.input_valid?
+  end
 end
