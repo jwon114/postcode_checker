be13ca1563e8600ff3fb19da7881e55f9e669ad5
jwon114 <jthgwong@gmail.com>
2020-02-05 13:20:15 +1300

    system tests

--- test/system/postcode_checkers_test.rb
+++ test/system/postcode_checkers_test.rb
@@ -20,14 +20,14 @@ class PostcodeCheckersTest < ApplicationSystemTestCase
     fill_in 'postcode', with: 'OX49 5NU'
     click_on 'submit_postcode'
 
-    assert_text "Postcode 'OX49 5NU' is NOT within your service area"
+    assert_text "Postcode 'OX495NU' is NOT within your service area"
   end
 
   test 'invalid postcode' do
     visit postcode_checker_url
     fill_in 'postcode', with: '123ABC'
     click_on 'submit_postcode'
-    
+
     assert_text 'Invalid postcode'
   end
 end
--- test/system/whitelisted_postcodes_test.rb
+++ test/system/whitelisted_postcodes_test.rb
@@ -12,7 +12,7 @@ class WhitelistedPostcodesTest < ApplicationSystemTestCase
     fill_in 'postcode', with: 'OX49 5NU'
     click_on 'add_postcode'
 
-    assert_text "New Postcode 'OX49 5NU' Whitelisted"
+    assert_text "New Postcode 'OX495NU' Whitelisted"
   end
 
   test 'attempt to add postcode that is already whitelisted' do
@@ -20,12 +20,18 @@ class WhitelistedPostcodesTest < ApplicationSystemTestCase
     fill_in 'postcode', with: 'sh24 1ab'
     click_on 'add_postcode'
 
-    assert_text "Postcode 'sh24 1ab' is already whitelisted"
+    assert_text "Postcode 'SH241AB' is already whitelisted"
   end
 
   test 'deleting postcode from whitelist' do
     visit whitelisted_postcodes_url
-    click_on 
+    whitelisted_postcodes = page.all(:css, 'table tbody tr')
+    postcode = whitelisted_postcodes.first.find(:css, 'td:first-child').text
+    first('table tbody tr').click_link('Delete')
+    new_whitelisted_postcodes = page.all(:css, 'table tbody tr')
+    
+    assert_text "Postcode '#{postcode}' removed"
+    assert_css 'table tbody tr', count: 1
   end
   
 end
